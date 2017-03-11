5cb7637fc059c0f24667c75ad4e98a70310e48f2
Brian Cerney <brianecerney@gmail.com>
2017-03-11 13:42:32 -0500

    Added first test for isValidStartTime.

--- src/main/java/com/briancerney/BabysitterKata.java
+++ src/main/java/com/briancerney/BabysitterKata.java
@@ -5,5 +5,9 @@ public class BabysitterKata {
 	public BabysitterKata() {
 		
 	}
+	
+	public boolean isValidStartTime(int startHour) {
+		return false;
+	}
 
 }
--- src/test/java/com/briancerney/BabysitterKataTest.java
+++ src/test/java/com/briancerney/BabysitterKataTest.java
@@ -1,6 +1,16 @@
 package com.briancerney;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
 public class BabysitterKataTest {
 	
+	@Test
+	public void isValidStartTimeReturnsFalseWhenPassed4() {
+		BabysitterKata babysitter = new BabysitterKata();
+		Assert.assertFalse(babysitter.isValidStartTime(4));
+	}
 
 }
