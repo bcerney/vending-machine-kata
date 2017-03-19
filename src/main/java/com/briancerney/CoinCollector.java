72d33d1aa86c8ad839177a7543d5d578f86e4357
Brian Cerney <brianecerney@gmail.com>
2017-03-19 14:57:23 -0400

    Added test setUp.

---
+++ src/main/java/com/briancerney/CoinCollector.java
@@ -0,0 +1,12 @@
+package com.briancerney;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoinCollector {
+	private List<Coin> currentCoins;
+	
+	public CoinCollector() {
+		this.currentCoins = new ArrayList<Coin>();
+	}
+}
---
+++ src/test/java/com/briancerney/CoinCollectorTest.java
@@ -0,0 +1,12 @@
+package com.briancerney;
+
+import org.junit.Test;
+
+public class CoinCollectorTest {
+	CoinCollector testCollector;
+	
+	@Test
+	public void setUp() {
+		testCollector = new CoinCollector();
+	}
+}
