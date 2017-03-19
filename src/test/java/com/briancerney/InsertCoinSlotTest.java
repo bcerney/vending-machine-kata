872189211bfced6457d1c0182e20b947b2832866
Brian Cerney <brianecerney@gmail.com>
2017-03-19 18:06:37 -0400

    Added test givenInsertCoinPassedQuarterDimeNickelAndPennyCurrentBalanceSizeIs3ReturnedCoinsSizeIs0.

--- src/test/java/com/briancerney/InsertCoinSlotTest.java
+++ src/test/java/com/briancerney/InsertCoinSlotTest.java
@@ -19,5 +19,16 @@ public class InsertCoinSlotTest {
 		Assert.assertEquals(1, testInsert.getNumberOfCurrentBalanceCoins());
 		Assert.assertEquals(0, testInsert.getNumberOfReturnedCoins());
 	}
+	
+	@Test
+	public void givenInsertCoinPassedQuarterDimeNickelAndPennyCurrentBalanceSizeIs3ReturnedCoinsSizeIs0() {
+		testInsert.insertCoin(Coin.QUARTER);
+		testInsert.insertCoin(Coin.DIME);
+		testInsert.insertCoin(Coin.NICKEL);
+		testInsert.insertCoin(Coin.PENNY);
+		
+		Assert.assertEquals(3, testInsert.getNumberOfCurrentBalanceCoins());
+		Assert.assertEquals(1, testInsert.getNumberOfReturnedCoins());
+	}
 
 }
