cc354491695c15e40874d4f236e719d3f2bb2045
Brian Cerney <brianecerney@gmail.com>
2017-03-19 17:30:05 -0400

    Added test givenAddCoinPassedPennyCoinReturnAddsPenny.

--- src/main/java/com/briancerney/CoinReturn.java
+++ src/main/java/com/briancerney/CoinReturn.java
@@ -1,5 +1,25 @@
 package com.briancerney;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoinReturn {
+	private List<Coin> returnedCoins;
+	
+	public CoinReturn() {
+		this.returnedCoins = new ArrayList<Coin>();
+	}
+	
+	public List<Coin> getReturnedCoins() {
+		return returnedCoins;
+	}
+	
+	public void addCoin(Coin coinToAdd) {
+		returnedCoins.add(coinToAdd);
+	}
+	
+	public int getNumberOfCoins() {
+		return returnedCoins.size();
+	}
 
 }
--- src/test/java/com/briancerney/CoinReturnTest.java
+++ src/test/java/com/briancerney/CoinReturnTest.java
@@ -1,5 +1,33 @@
 package com.briancerney;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CoinReturnTest {
+	CoinReturn testReturn;
+
+	@Before
+	public void setUp() {
+		testReturn = new CoinReturn();
+	}
+
+	@Test
+	public void givenAddCoinPassedPennyCoinReturnAddsPenny() {
+		// TODO: logic shouldn't be in test, consider creating class 
+		// to house both CoinCollector and CoinReturn
+		CoinCollector testCollect = new CoinCollector();
+		Coin testPenny = Coin.PENNY;
+		boolean addTest = testCollect.addCoin(testPenny);
+		if (!addTest) {testReturn.addCoin(testPenny);}
+		List<Coin> coinsReturned = testReturn.getReturnedCoins();
+		Coin shouldBePenny = coinsReturned.get(0);
+		
+		
+		Assert.assertEquals(1, testReturn.getNumberOfCoins());
+		Assert.assertEquals(Coin.PENNY, shouldBePenny);
+	}
 
 }
