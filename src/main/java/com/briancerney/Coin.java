94b15f379a1a88b1eaaf9d204cbe50ca126aa410
Brian Cerney <brianecerney@gmail.com>
2017-03-19 14:38:27 -0400

    Added test givenSetWeightInGrams6SetDiameterInMm24CoinGettersReturnCorrectValues.

---
+++ src/main/java/com/briancerney/Coin.java
@@ -0,0 +1,29 @@
+package com.briancerney;
+
+public class Coin {
+	private int weightInGrams;
+	private int diameterInMm;
+	
+	public Coin() {
+
+	}
+
+	public int getWeightInGrams() {
+		return weightInGrams;
+	}
+
+	public void setWeightInGrams(int weightInGrams) {
+		this.weightInGrams = weightInGrams;
+	}
+
+	public int getDiameterInMm() {
+		return diameterInMm;
+	}
+
+	public void setDiameterInMm(int diameterInMm) {
+		this.diameterInMm = diameterInMm;
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/briancerney/CoinTest.java
@@ -0,0 +1,19 @@
+package com.briancerney;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CoinTest {
+	Coin testCoin;
+	
+	@Test
+	public void givenSetWeightInGrams6SetDiameterInMm24CoinGettersReturnCorrectValues() {
+		testCoin = new Coin();
+		testCoin.setWeightInGrams(6);
+		testCoin.setDiameterInMm(24);
+		
+		Assert.assertEquals(6, testCoin.getWeightInGrams());
+		Assert.assertEquals(24, testCoin.getDiameterInMm());
+	}
+
+}
